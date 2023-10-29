7a3097b870a3b747fd9abf6cc7cc912d0c6e32be
RealChoki <123271772+RealChoki@users.noreply.github.com>
2023-10-29 21:34:01 +0100

    comments

--- JavaScript/Counter/index.js
+++ JavaScript/Counter/index.js
@@ -1,19 +1,17 @@
-let count = 0
+let count = 0; // initialize count variable to 0
 
-document.getElementById("increment-btn").addEventListener("click",()=>{
-    count += 1
-    document.getElementById("count-el").textContent = count
-}) 
+document.getElementById("increment-btn").addEventListener("click", () => {
+  count += 1; // increment count variable by 1
+  document.getElementById("count-el").textContent = count; // update count element with new count value
+});
 
-document.getElementById("save-btn").addEventListener("click",()=>{
-    const saveEl = document.getElementById("save-el")
-    if( saveEl.textContent == "Previous amount of people:"){
-        saveEl.textContent += ` ${count} `
-    } else{
-        saveEl.textContent += `- ${count} `
-    }
-    document.getElementById("count-el").textContent = 0
-    count = 0
-})
-
-
+document.getElementById("save-btn").addEventListener("click", () => {
+  const saveEl = document.getElementById("save-el");
+  if (saveEl.textContent == "Previous amount of people:") {
+    saveEl.textContent += ` ${count} `; // append current count value to save element
+  } else {
+    saveEl.textContent += `- ${count} `; // append current count value to save element with a dash separator
+  }
+  document.getElementById("count-el").textContent = 0; // reset count element to 0
+  count = 0; // reset count variable to 0
+});
